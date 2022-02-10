f344bc092f0e5a7d723e324ecb742e38fb165b7d
Rammel <rammel.balagtas@gmail.com>
2022-02-10 11:21:17 -0500

    Task 10: Add API endpoint for Addition operation

---
+++ Calculator-WebAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,25 @@
+﻿using Calculator_ClassLibrary;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator_WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")] 
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+
+        //Method for the Add endpoint
+        [HttpGet]
+        public int? Add(int lValue, int rValue)
+        {
+            //Allow cross browser access of API
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            return Calculator.Add(lValue, rValue);
+        }
+    }
+}
