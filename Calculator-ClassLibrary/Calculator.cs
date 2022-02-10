23bb52494d7f7d379bfb8d05fce5f65c8f217b26
Rammel <rammel.balagtas@gmail.com>
2022-02-09 23:18:30 -0500

    Task 5: Add method for performing division

--- Calculator-ClassLibrary/Calculator.cs
+++ Calculator-ClassLibrary/Calculator.cs
@@ -22,8 +22,19 @@ namespace Calculator_ClassLibrary
             return lValue * rValue;
         }
 
-
-
+        // Method for dividing 2 integers
+        public static double Divide(int lValue, int rValue)
+        {
+            if (rValue != 0)
+            {
+                return lValue / rValue;
+            } 
+            else
+            {
+                //throw exception for dividing by zero
+                throw new DivideByZeroException();
+            }
+        }
 
     }
 }
