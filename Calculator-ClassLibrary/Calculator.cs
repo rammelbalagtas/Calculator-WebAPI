dd19937b3e50ef94f36a95446d337b36b704769a
Rammel <rammel.balagtas@gmail.com>
2022-02-09 23:11:50 -0500

    Task 2: Add method for performing addition

---
+++ Calculator-ClassLibrary/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculator_ClassLibrary
+{
+    public static class Calculator
+    {
+        // Method for adding 2 integers
+        public static double Add(int lValue, int rValue)
+        {
+            return lValue + rValue;
+        }
+    }
+}
--- Calculator-ClassLibrary/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Calculator_ClassLibrary
-{
-    public class Class1
-    {
-    }
-}
