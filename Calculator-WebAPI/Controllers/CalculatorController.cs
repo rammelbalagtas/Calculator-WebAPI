580a65287e1e9e0f851ccdcc3c2d7ca8684205aa
Rammel <rammel.balagtas@gmail.com>
2022-02-10 11:37:26 -0500

    Task 13: Add API endpoint for Division operation

--- Calculator-WebAPI/Controllers/CalculatorController.cs
+++ Calculator-WebAPI/Controllers/CalculatorController.cs
@@ -15,7 +15,7 @@ namespace Calculator_WebAPI.Controllers
 
         //Method for the Add endpoint
         [HttpGet]
-        public int? Add(int lValue, int rValue)
+        public int Add(int lValue, int rValue)
         {
             //Allow cross browser access of API
             Response.Headers.Add("Access-Control-Allow-Origin", "*");
@@ -24,7 +24,7 @@ namespace Calculator_WebAPI.Controllers
 
         //Method for the Subtract endpoint
         [HttpGet]
-        public int? Subtract(int lValue, int rValue)
+        public int Subtract(int lValue, int rValue)
         {
             //Allow cross browser access of API
             Response.Headers.Add("Access-Control-Allow-Origin", "*");
@@ -33,11 +33,31 @@ namespace Calculator_WebAPI.Controllers
 
         //Method for the Multiplication endpoint
         [HttpGet]
-        public int? Multiply(int lValue, int rValue)
+        public int Multiply(int lValue, int rValue)
         {
             //Allow cross browser access of API
             Response.Headers.Add("Access-Control-Allow-Origin", "*");
             return Calculator.Multiply(lValue, rValue);
+        }
+
+        //Method for the Divide endpoint
+        [HttpGet]
+        public double? Divide(int lValue, int rValue)
+        {
+            //Allow cross browser access of API
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+            //Try-catch block for division, return status code 400 in case of exception (e.g. DivideByZeroException)
+            try
+            {
+                return Calculator.Divide(lValue, rValue);
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 400;
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "Bad Request";
+                return null;
+            }
         }
     }
 }
