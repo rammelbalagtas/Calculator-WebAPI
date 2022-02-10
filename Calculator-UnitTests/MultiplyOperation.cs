e32a3e7ae92e7a9a8d8adee2d14165483e34d887
Rammel <rammel.balagtas@gmail.com>
2022-02-10 10:14:24 -0500

    Task 8: Create tests for library task: Multiplication

---
+++ Calculator-UnitTests/MultiplyOperation.cs
@@ -0,0 +1,44 @@
+﻿using Calculator_ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator_UnitTests
+{
+    [TestClass]
+    public class MultiplyOperation
+    {
+        [TestMethod]
+        [DataRow(1, 2, 2)]
+        [DataRow(10, 12, 120)]
+        [DataRow(100, 200, 20000)]
+        public void MultiplyOperation_InputTwoPositiveNumbers_ReturnProduct(int lValue, int rValue, int expected)
+        {
+            Assert.AreEqual(expected, Calculator.Multiply(lValue, rValue));
+        }
+
+        [TestMethod]
+        [DataRow(1, -2)]
+        [DataRow(-10, 12)]
+        [DataRow(100, -200)]
+        public void MultiplyOperation_InputOnePositiveOneNegative_ReturnProductWithNegativeSign(int lValue, int rValue)
+        {
+            Assert.IsTrue(Calculator.Multiply(lValue, rValue) < 0);
+        }
+
+        [TestMethod]
+        [DataRow(-1, -2)]
+        [DataRow(-10, -12)]
+        [DataRow(-100, -200)]
+        public void MultiplyOperation_InputTwoNegativeNumbers_ReturnProductWithPositiveSign(int lValue, int rValue)
+        {
+            Assert.IsTrue(Calculator.Multiply(lValue, rValue) > 0);
+        }
+
+        [TestMethod]
+        [DataRow(-1, 0)]
+        [DataRow(0, 12)]
+        public void MultiplyOperation_MultiplyZeroByAnyNumber_ReturnProducEqualToZero(int lValue, int rValue)
+        {
+            Assert.IsTrue(Calculator.Multiply(lValue, rValue) == 0);
+        }
+    }
+}
